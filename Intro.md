cf89d7ccf699951586ce360f2e1cecad3905ad7d
johnny-alibuyog <johnny.alibuyog@gmail.com>
2025-04-22 22:35:36 -0400

    Initial DYI implmentation of event sourcing

---
+++ Intro.md
@@ -0,0 +1,9 @@
+1. Event Sourcing
+2. Motivation
+3. Domain
+4. Event Store
+5. Projections
+6. Behaviour
+7. Testing
+
+Events are fundamental for business processes.
---
+++ Program.fs
@@ -0,0 +1,78 @@
+﻿module Infrastructure =
+    type EventStore<'Event> = {
+        get: unit -> 'Event list
+        append: 'Event list -> unit
+    }
+
+    module EventStore =
+        type Message<'Event> =
+            | Append of 'Event list
+            | Get of AsyncReplyChannel<'Event list>
+
+        let initialize () : EventStore<'Event> =
+            
+            // Create a mailbox processor to handle the events
+            let agent = MailboxProcessor.Start(fun inbox ->            
+                let rec loop history = async {
+                    let! message = inbox.Receive()
+
+                    match message with
+                    
+                    | Append events -> 
+                        return! loop (history @ events)
+                    
+                    | Get channel ->
+                        channel.Reply history
+                        return! loop history
+                }
+
+                loop []
+            )
+
+            let append events =
+                agent.Post (Append events)
+
+            let get () =
+                agent.PostAndReply Get
+
+            { get = get; append = append }
+
+
+module Domain =
+
+    type Flavour =
+        | Strawberry
+        | Vanilla
+
+    type Event =
+        | Flavour_sold of flavour: Flavour
+        | Flavour_restocked of flavour: Flavour * quantity: int
+        | Flavour_went_out_of_stock of flavour: Flavour
+        | Flavour_was_not_in_stock of flavour: Flavour
+
+
+module Helpler =
+    let private printUnorderList list =
+        list
+        |> List.iteri (fun i e -> printfn "%d: %A" i e)
+
+    let printEvents events =
+        events
+        |> List.length
+        |> printfn "History (Length: %i)"
+
+        events
+        |> printUnorderList
+
+
+open Infrastructure
+open Domain
+open Helpler
+
+let store = EventStore.initialize()
+
+store.append [ Flavour_restocked (Vanilla, 3) ]
+store.append [ Flavour_sold Vanilla ]
+store.append [ Flavour_sold Vanilla ]
+store.append [ Flavour_sold Vanilla; Flavour_went_out_of_stock Vanilla ]
+store.get() |> printEvents
